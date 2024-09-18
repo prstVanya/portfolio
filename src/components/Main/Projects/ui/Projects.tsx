4685557ef2caf218a11b6f398962993024762b66
Ivan <vany4golos@yandex.ru>
2024-09-18 15:43:06 +0300

    test

--- src/components/Main/Projects/ui/Projects.tsx
+++ src/components/Main/Projects/ui/Projects.tsx
@@ -1,11 +1,44 @@
+import SpliteText from 'utils/spliteText/spliteText';
 import cls from './Projects.module.css';
 import { classNames } from 'utils/classNames/classNames';
 
 export const Projects = () => {
   return (
-    <section className={classNames(cls.section, {}, [])}>
+    <section className={classNames(cls.education, {}, [])}>
       <div className={classNames(cls.mainContainer, {}, [])}>
-        <h1 className='class'>Hello</h1>
+        <SpliteText>Education</SpliteText>
+        <ul className={classNames(cls.block, {}, [])}>
+          <li className={classNames(cls.card, {}, [])}>
+            <h3 className={classNames(cls.heading, {}, [])}>CFU</h3>
+            <p className={classNames(cls.subtitle, {}, [])}>
+              Main university in Crimea
+            </p>
+            <div className={classNames(cls.info, {}, [])}>
+              <div className={classNames(cls.logo, {}, [])}>
+                <img />
+              </div>
+              <a 
+                href='https://cfuv.ru/?ysclid=m17sdd4udt251873995' 
+                target='_blank' 
+                className={classNames(cls.link, {}, [])}>Link to site</a>
+            </div>
+          </li>
+          <li className={classNames(cls.card, {}, [])}>
+            <h3 className={classNames(cls.heading, {}, [])}>Yandex Practicum</h3>
+            <p className={classNames(cls.subtitle, {}, [])}>
+              Additional training in the web developer course
+            </p>
+            <div className={classNames(cls.info, {}, [])}>
+              <div className={classNames(cls.logo, {}, [])}>
+                <img />
+              </div>
+              <a 
+                href='https://practicum.yandex.ru' 
+                target='_blank' 
+                className={classNames(cls.link, {}, [])}>Link to site</a>
+            </div>
+          </li>
+        </ul>
       </div>
     </section>
   )
