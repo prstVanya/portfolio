f331fef600248adcc529c616f1ac66ceb385a0a2
Ivan <vany4golos@yandex.ru>
2024-09-18 20:26:49 +0300

    fix: linter

--- src/components/Footer/ui/Footer.tsx
+++ src/components/Footer/ui/Footer.tsx
@@ -1,13 +1,13 @@
-import cls from './Footer.module.css';
 import { classNames } from 'utils/classNames/classNames';
 
 import Steam from 'assets/images/Footer/steam.svg';
 import Vk from 'assets/images/Footer/vk.svg';
 import Telegram from 'assets/images/Footer/telegram.svg';
+import cls from './Footer.module.css';
 
 interface IFooterData {
   className?: string;
-};
+}
 
 export const Footer = ({ className }: IFooterData) => {
   const date = new Date();
@@ -16,29 +16,29 @@ export const Footer = ({ className }: IFooterData) => {
   return (
     <footer className={classNames(cls.footer, {}, [className])}>
       <nav className={classNames(cls.links, {}, [])}>
-        <a 
+        <a
           className={classNames(cls.link, {}, [])}
           target='_blank'
-          href='https://steamcommunity.com/id/674931'
+          href='https://steamcommunity.com/id/674931' rel="noreferrer"
         >
           <Steam />
         </a>
-        <a 
+        <a
           className={classNames(cls.link, {}, [])}
           target='_blank'
-          href='https://vk.com/ibolvan123'
+          href='https://vk.com/ibolvan123' rel="noreferrer"
         >
           <Vk />
         </a>
-        <a 
+        <a
           className={classNames(cls.link, {}, [])}
           target='_blank'
-          href='https://t.me/vanyaChern'
+          href='https://t.me/vanyaChern' rel="noreferrer"
         >
           <Telegram />
         </a>
       </nav>
       <p className={classNames(cls.paragraph, {}, [])}>{`${'@ VanyaGachist'} ${year}`}</p>
     </footer>
-  )
-}
+  );
+};
