c3d9f306d50c5620273866864096f7fc7ede1d8a
Ivan <vany4golos@yandex.ru>
2024-10-15 16:08:55 +0300

    feat: new template

--- src/components/Main/Statistics/ui/Statistics.tsx
+++ src/components/Main/Statistics/ui/Statistics.tsx
@@ -1,5 +1,6 @@
 import { classNames } from 'utils/classNames/classNames';
 import Age from 'utils/stats/stats';
+import { CommandBlock } from 'components/Main/CommandBlock';
 import cls from './Statistics.module.css';
 
 interface IStatisticsData {
@@ -10,30 +11,37 @@ export const Statistics = ({ className }: IStatisticsData) => {
   return (
       <div className={classNames(cls.moreInfo, {}, [className])}>
         <h3 className={classNames(cls.statsHeading)}>Statistics</h3>
-        <ul className={classNames(cls.stats, {}, [])}>
-          <li className={classNames(cls.statsItem, {}, [])}>
-            <p className={classNames(cls.current)}>Current Age</p>
-            <p className={classNames(cls.current, {}, [])}>
-              <Age />
-            </p>
-          </li>
-          <li className={classNames(cls.statsItem, {}, [])}>
-            <p className={classNames(cls.current)}>Main City</p>
-            <p className={classNames(cls.current, {}, [])}>Simferopol</p>
-          </li>
-          <li className={classNames(cls.statsItem, {}, [])}>
-            <p className={classNames(cls.current)}>SoftSkills</p>
-            <p className={classNames(cls.current, {}, [])}>50%</p>
-          </li>
-          <li className={classNames(cls.statsItem, {}, [])}>
-            <p className={classNames(cls.current)}>HardSkills</p>
-            <p className={classNames(cls.current, {}, [])}>???</p>
-          </li>
-          <li className={classNames(cls.statsItem, {}, [])}>
-            <p className={classNames(cls.current)}>Steam Games</p>
-            <p className={classNames(cls.current, {}, [])}>79</p>
-          </li>
-        </ul>
+        <CommandBlock>
+          <p className={classNames(cls.text, {}, [])}>
+            <span className={classNames(cls.span, {}, [])}>
+              prstvanya $
+            </span> cat statistics
+          </p>
+            <ul className={classNames(cls.stats, {}, [])}>
+              <li className={classNames(cls.statsItem, {}, [])}>
+                <p className={classNames(cls.text, {}, [])}>Current Age</p>
+                <p className={classNames(cls.text, {}, [])}>
+                  <Age />
+                </p>
+              </li>
+              <li className={classNames(cls.statsItem, {}, [])}>
+                <p className={classNames(cls.text)}>Main City</p>
+                <p className={classNames(cls.text, {}, [])}>Simferopol</p>
+              </li>
+              <li className={classNames(cls.statsItem, {}, [])}>
+                <p className={classNames(cls.text)}>SoftSkills</p>
+                <p className={classNames(cls.text, {}, [])}>50%</p>
+              </li>
+              <li className={classNames(cls.statsItem, {}, [])}>
+                <p className={classNames(cls.text)}>HardSkills</p>
+                <p className={classNames(cls.text, {}, [])}>???</p>
+              </li>
+              <li className={classNames(cls.statsItem, {}, [])}>
+                <p className={classNames(cls.text)}>Steam Games</p>
+                <p className={classNames(cls.text, {}, [])}>79</p>
+              </li>
+            </ul>
+        </CommandBlock>
       </div>
   );
 };
