f5804eae9e08a7c8617cac7a998efaf46d936652
Ivan <vany4golos@yandex.ru>
2024-11-01 10:19:16 +0100

    feat: ResumeLink

--- src/components/Footer/ui/Footer.tsx
+++ src/components/Footer/ui/Footer.tsx
@@ -3,6 +3,7 @@ import { classNames } from 'utils/classNames/classNames';
 import Steam from 'assets/images/Footer/steam.svg';
 import Vk from 'assets/images/Footer/vk.svg';
 import Telegram from 'assets/images/Footer/telegram.svg';
+import Download from 'assets/images/icons/download.svg';
 import cls from './Footer.module.css';
 
 interface IFooterData {
@@ -15,30 +16,40 @@ export const Footer = ({ className }: IFooterData) => {
 
   return (
     <footer className={classNames(cls.footer, {}, [className])}>
-      <nav className={classNames(cls.links, {}, [])}>
-        <a
-          className={classNames(cls.link, {}, [])}
-          target='_blank'
-          href='https://steamcommunity.com/id/674931' rel="noreferrer"
-        >
-          <Steam />
-        </a>
-        <a
-          className={classNames(cls.link, {}, [])}
-          target='_blank'
-          href='https://vk.com/ibolvan123' rel="noreferrer"
-        >
-          <Vk />
-        </a>
-        <a
-          className={classNames(cls.link, {}, [])}
-          target='_blank'
-          href='https://t.me/vanyaChern' rel="noreferrer"
-        >
-          <Telegram />
-        </a>
-      </nav>
-      <p className={classNames(cls.paragraph, {}, [])}>{`${'@ VanyaGachist'} ${year} 🍺`}</p>
+      <div className={classNames(cls.block, {}, [])}>
+        <nav className={classNames(cls.links, {}, [])}>
+          <a
+            className={classNames(cls.link, {}, [])}
+            target='_blank'
+            href='https://steamcommunity.com/id/674931' rel="noreferrer"
+          >
+            <Steam />
+          </a>
+          <a
+            className={classNames(cls.link, {}, [])}
+            target='_blank'
+            href='https://vk.com/ibolvan123' rel="noreferrer"
+          >
+            <Vk />
+          </a>
+          <a
+            className={classNames(cls.link, {}, [])}
+            target='_blank'
+            href='https://t.me/vanyaChern' rel="noreferrer"
+          >
+            <Telegram />
+          </a>
+        </nav>
+        <p className={classNames(cls.paragraph, {}, [])}>{`${'@ VanyaGachist'} ${year} 🍺`}</p>
+      </div>
+      <a
+        className={classNames(cls.resume, {}, [])}
+        target='_blank'
+        href='https://positive-reptile-e2d.notion.site/111b52bac8bb801982c2f25e14c14d70' rel="noreferrer"
+      >
+        <p className={classNames(cls.textB, {}, [])}>Resume</p>
+        <Download />
+      </a>
     </footer>
   );
 };
